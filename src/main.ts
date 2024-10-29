c9b9c90a2a2487ccf3176b88732cad07d7cafe28
denh <d3n4123@gmail.com>
2024-10-29 11:35:46 -0600

    se cambio el main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzksLffdOrGcMMFJXMZJAiv6MaZxKlLug",
+  authDomain: "lista-de-alimentos.firebaseapp.com",
+  databaseURL: "https://lista-de-alimentos-default-rtdb.firebaseio.com",
+  projectId: "lista-de-alimentos",
+  storageBucket: "lista-de-alimentos.appspot.com",
+  messagingSenderId: "465265602418",
+  appId: "1:465265602418:web:c7bc8dfa9cf0f3ac5037eb",
+  measurementId: "G-F8GV7QWMCB"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
